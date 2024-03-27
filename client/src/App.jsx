77671e9fb548c8f0dcd2f485d4129555f2bc696b
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-03-27 13:11:58 +0530

    services page not responsive for now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Section1 from './sections/Section1'
+import Services from './sections/Services'
 function App() {
 
   return (
     <>
     <Navbar/>
     <Section1/>
+    <Services/>
     </>
   )
 }
