0daa2b710020d9cc62b8945b7adc557d7ad3eecf
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-03-31 10:21:40 +0530

    frontend done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Navbar/>
     <Routes>
     <Route path="/" element={<Home/>} />
-    <Route path="upload" element={<Upload/>} />
+    <Route path="/upload" element={<Upload/>} />
     </Routes>
     <Footer/>
     </BrowserRouter>
